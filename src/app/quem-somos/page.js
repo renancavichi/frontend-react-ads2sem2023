7fdc20b37143dd1414b7aed9536e9c41ab7fc72f
Renan Cavichi <renancavichi@gmail.com>
2023-08-29 22:35:19 -0300

    create header component

---
+++ src/app/quem-somos/page.js
@@ -0,0 +1,9 @@
+
+
+const QuemSomos = () => {
+  return (
+    <h1>Bem vindo ao Quem Somos!</h1>
+  )
+}
+
+export default QuemSomos
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header id="header">
+        <h1>Logo</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Quem Somos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
