cd3d5489ca5c3c02ad392989297591efbb6660c1
Renan Cavichi <renancavichi@gmail.com>
2023-09-19 21:58:12 -0300

    add useState showCard

--- src/components/cards/CardUser.js
+++ src/components/cards/CardUser.js
@@ -1,40 +1,58 @@
 'use client'
 
+import {useState} from 'react'
 import './CardUser.css'
 
-const handleDelete = async (idUser) => {
+const CardUser = ({user}) => {
 
-  const objUser = {
-    id: idUser
+  const [showCard, setShowCard] = useState(true)
+
+  const handleDelete = async (idUser) => {
+
+    const objUser = {
+      id: idUser
+    }
+  
+    const response = await fetch('http://localhost:3300/user',
+      {
+        cache: 'no-store',
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(objUser)
+      }
+    )
+    
+    if(response.ok){
+      const result = await response.json()
+      if(result?.success){
+        setShowCard(false)
+      }
+    }
   }
 
-  const response = await fetch('http://localhost:3300/user',
-    {
-      cache: 'no-store',
-      method: 'DELETE',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(objUser)
-    }
+  
+  return (
+    <>
+      {
+        showCard ?
+        <div className="card-user">
+        <img src={user.photo} alt={user.name} width="70px" height="70px" />
+          <div>
+              <h4>{user.name}</h4>
+              <p>{user.email}</p>
+              <p>
+                <span className="delete" onClick={() => handleDelete(user.id)}>Excluir</span>
+                <span>Editar</span>
+              </p>
+          </div>
+        </div>
+      : null
+      }
+    </>
   )
-  console.log(await response.json())
 }
 
-const CardUser = ({user}) => {
-  return (
-    <div className="card-user">
-        <img src={user.photo} alt={user.name} width="70px" height="70px" />
-        <div>
-            <h4>{user.name}</h4>
-            <p>{user.email}</p>
-            <p>
-              <span className="delete" onClick={() => handleDelete(user.id)}>Excluir</span>
-              <span>Editar</span>
-            </p>
-        </div>
-    </div>
-  )
-}
 
 export default CardUser
