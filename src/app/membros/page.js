00c7954c6f43d7516bb9e96d007c25bbd1e3fc74
Renan Cavichi <renancavichi@gmail.com>
2023-09-26 20:39:35 -0300

    start modal template

--- src/app/membros/page.js
+++ src/app/membros/page.js
@@ -1,11 +1,17 @@
 import Header from "@/components/layout/Header"
 import ListMembers from "@/components/utils/ListMembers"
+import '@/components/utils/Modal.css'
 
 const Membros = async () => {
   return (
     <>
         <Header />
         <ListMembers />
+        <div className="modal">
+          <main>
+            <h1>Exemplo Modal</h1>
+          </main>
+        </div>
     </>
   )
 }
